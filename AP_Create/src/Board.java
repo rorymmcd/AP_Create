2baac2a2fc39c63aa612c24a798c83f7c2aac755
0001064272 <0001064272@LAT-B8ZLB32.students.ccisd.net>
2018-04-02 13:53:10 -0500

    created square and board classes

---
+++ AP_Create/src/Board.java
@@ -0,0 +1,8 @@
+
+public class Board {
+	private Square[][] squares;
+	
+	public Board(){
+		squares = new Square[10][10];
+	}
+}
---
+++ AP_Create/src/Square.java
@@ -0,0 +1,13 @@
+
+public class Square {
+	public int shipID, shipPart;
+	public Square(){
+		shipID = -1;
+		shipPart = -1;
+	}
+	
+	public void Set(int a, int b){
+		shipID = a;
+		shipPart = b;
+	}
+}
