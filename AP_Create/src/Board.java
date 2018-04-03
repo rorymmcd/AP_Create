c9faa9266a7596dd82d82dff1ad9ee7413997ef1
0001064272 <0001064272@CLHS-2024-26-S.students.ccisd.net>
2018-04-03 14:27:38 -0500

    ship placment

--- AP_Create/src/Board.java
+++ AP_Create/src/Board.java
@@ -5,4 +5,20 @@ public class Board {
 	public Board(){
 		squares = new Square[10][10];
 	}
+	
+	public boolean setShip(int x,int y,int o){
+		// o  is orientation, 0 is horizontal, 1 is vertical 
+		if(x>9||y>9)return false;
+		if(!(o==0||o==1))return false;
+		if(o==0&&x>6)return false;
+		if(o==1&&y>6)return false;
+		if(o==0&&((squares[x][y].shipID!=-1)||
+				(squares[x+1][y].shipID!=-1)||
+				(squares[x+2][y].shipID!=-1)||
+				(squares[x+3][y].shipID!=-1)))return false;
+		if(o==1&&((squares[x][y].shipID!=-1)||
+				(squares[x][y+1].shipID!=-1)||
+				(squares[x][y+2].shipID!=-1)||
+				(squares[x][y+3].shipID!=-1)))return false;
+	}
 }
--- AP_Create/src/Square.java
+++ AP_Create/src/Square.java
@@ -1,6 +1,7 @@
 
 public class Square {
 	public int shipID, shipPart;
+	public boolean beenHit;
 	public Square(){
 		shipID = -1;
 		shipPart = -1;
@@ -9,5 +10,6 @@ public class Square {
 	public void Set(int a, int b){
 		shipID = a;
 		shipPart = b;
+		beenHit = false;
 	}
 }
